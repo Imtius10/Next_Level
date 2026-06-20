45da5ece1da75d7af6e5181d312193aa0cbcd4c0
Imtius Ahmad <h.imtius10@gmail.com>
2026-06-20 21:56:03 +0600

    Add SQL queries for matches and bookings
    Added multiple SQL queries for match and booking data retrieval.

--- Assigment_3/QUERY.sql
+++ Assigment_3/QUERY.sql
@@ -79,3 +79,65 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+--1
+select match_id,
+  fixture,
+  base_ticket_price 
+  from matches 
+  where tournament_category='Champions League' 
+  and match_status='Available'
+--2
+SELECT user_id,
+       full_name,
+       email
+FROM Users
+WHERE full_name ILIKE 'Tanvir%'
+   OR full_name ILIKE '%Haque%';
+--3
+SELECT booking_id,
+       user_id,
+       match_id,
+       COALESCE(payment_status, 'Action Required')
+           AS systematic_status
+FROM Bookings
+WHERE payment_status IS NULL;
+
+--4
+SELECT b.booking_id,
+       u.full_name,
+       m.fixture,
+       b.total_cost
+FROM Bookings b
+INNER JOIN Users u
+    ON b.user_id = u.user_id
+INNER JOIN Matches m
+    ON b.match_id = m.match_id;
+
+--5
+SELECT u.user_id,
+       u.full_name,
+       b.booking_id
+FROM Users u
+LEFT JOIN Bookings b
+    ON u.user_id = b.user_id
+ORDER BY u.user_id;
+
+
+--6
+SELECT booking_id,
+       match_id,
+       total_cost
+FROM Bookings
+WHERE total_cost >
+(
+    SELECT AVG(total_cost)
+    FROM Bookings
+);
+
+--7
+SELECT match_id,
+       fixture,
+       base_ticket_price
+FROM Matches
+ORDER BY base_ticket_price DESC
+LIMIT 2 OFFSET 1;
